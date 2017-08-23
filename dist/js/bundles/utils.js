13069de1b59aacb13addfb01a45923ca3cb2cbc2
Jonni Lundy <jonathan.lundy@liferay.com>
2017-08-23 10:55:01 -0700

    build

---
+++ dist/js/bundles/utils.js
@@ -0,0 +1,28 @@
+var pageComponent =
+webpackJsonppageComponent([202],{
+
+/***/ 31:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.dispatchGlobalState = dispatchGlobalState;
+function dispatchGlobalState() {
+	if (window.electricPageComponent) {
+		try {
+			window.electricPageComponent.setState({
+				element: '#pageComponent',
+				page: page,
+				site: data.site
+			});
+		} catch (error) {}
+	}
+}
+
+/***/ })
+
+},[31]);
